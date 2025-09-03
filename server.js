70e62dd63991595669ed8ab05f29e64f7ba5bd33
Inian Antony <inianantony@users.noreply.github.com>
2025-09-03 15:17:53 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+  // Always serve index.html
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // 404 for anything else
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+}).listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
